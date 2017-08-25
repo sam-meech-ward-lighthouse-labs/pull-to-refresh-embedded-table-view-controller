e43dadf1ab7e58135312a7345c3249d3748a6430
Sam Meech-Ward <sam@meech-ward.com>
2017-08-25 12:30:19 -0700

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Sam Meech-Ward on 2017-08-25.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by Sam Meech-Ward on 2017-08-25.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
